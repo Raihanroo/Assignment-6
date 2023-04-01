c90a93f5f288b78c095b6d873160c3fd5ce0a6f7
Raihanroo <raihanroo21@gmail.com>
2023-04-01 19:25:43 +0300

    the third

--- Assignment.js
+++ Assignment.js
@@ -1,35 +1,66 @@
-const loadApi = () =>{
-    fetch('https://openapi.programming-hero.com/api/ai/tools')
+const loadApi = () => {
+  fetch('https://openapi.programming-hero.com/api/ai/tools')
     .then(res => res.json())
     .then(data => displayApi(data.data.tools));
 }
 
 
-const displayApi = data =>{
-    // console.log(data);
-    // step 1 : container element
-    const toolscontainer = document.getElementById('tools-container');
-    data.forEach(tool =>{
-        console.log(tool)
-        // step 2: create child for each element
-        const toolDiv = document.createElement('div');
-        toolDiv.classList.add('col');
-        // step-3 set content of the child 
+const displayFeature = (data) => {
+  let featureList = '';
+  for (const singleData of data) {
+    featureList += `<li>${singleData}</li>`
+  }
+  return featureList;
+}
 
-        toolDiv.innerHTML = `
+const displayApi = data => {
+  // console.log(data);
+  // step 1 : container element
+  const toolscontainer = document.getElementById('tools-container');
+  data.forEach(tool => {
+    console.log(tool)
+    // step 2: create child for each element
+    const toolDiv = document.createElement('div');
+    toolDiv.classList.add('col');
+    // step-3 set content of the child 
+
+    toolDiv.innerHTML = `
         <div class="card h-100">
         <img src="${tool.image}" class="card-img-top" alt="...">
+          <ol class="feature-container">
+        
+          </ol>
         <div class="card-body">
-          <h5 class="card-title">${tool.name}</h5>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <p class="card-text">
+          <h5>Feature</h5>
+            <ol>
+              ${displayFeature(tool.features)}
+            </ol>
+          </p>
+          <hr>
+          <div class="d-flex justify-content-between">
+            <div >
+              <h5 class="card-title">${tool.name}</h5>
+              <h6>  ${tool.published_in}</h6>
+            </div>
+            <button onclick = showDetail(${tool}) >Details</button>
+          </div>
         </div>
+        
       </div>
         `
+    // const featureList = document.createElement('li');
 
-        // step-4: appendChild
-        
-        toolscontainer.appendChild(toolDiv);
-    })
+    // tool.features.forEach(item =>{
+    //   featureList.innerHTML = `
+    //   <li>${item}</li>
+    //   `
+    // })
+    // document.querySelector(".feature-container").appendChild(featureList);
+    // step-4: appendChild
+
+    toolscontainer.appendChild(toolDiv);
+  })
 }
 
 loadApi();
