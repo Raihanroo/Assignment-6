5be881a23e9ae70c8b75d00442df979139a3e87c
Raihanroo <raihanroo21@gmail.com>
2023-04-07 11:52:27 +0300

    ten

--- Assignment.js
+++ Assignment.js
@@ -1,8 +1,25 @@
 const loadApi = () => {
   fetch('https://openapi.programming-hero.com/api/ai/tools')
     .then(res => res.json())
-    .then(data => displayApi(data.data.tools));
+    .then(data => displayApi(data.data.tools.slice(0,6)));
 }
+
+// shortDate button API work
+const sortDate = async () =>{  // async await function 
+  const url = `https://openapi.programming-hero.com/api/ai/tools`
+  const loadData = await fetch(url);
+  const reciveData = await loadData.json()
+  const daTa = reciveData.data.tools
+  console.log(daTa);
+  daTa.sort((a,b) =>{
+    return new Date(a.published_in) - new Date(b.published_in);
+  
+  })
+  console.log(daTa);
+
+  displayApi(daTa);
+}
+
 
 
 const displayFeature = (data) => {
@@ -13,6 +30,21 @@ const displayFeature = (data) => {
   return featureList;
 }
 
+
+// see more button
+const showAllUniverses =  () => {
+  document.getElementById('spinner').classList.remove('d-none'); //spinee remove
+  document.getElementById('spinner').classList.add('d-block');  // spinner step-2
+  const url = `https://openapi.programming-hero.com/api/ai/tools`
+  fetch(url)
+  .then(res => res.json())
+  .then(data => displayApi(data.data.tools));
+
+  // show all button 
+const button = document.getElementById("button-all");
+button.classList.add("d-none");
+
+}
 
 const modalDataPush = data =>{
   console.log(data.data);
@@ -33,7 +65,7 @@ const modalDataPush = data =>{
 
 const modalCost = document.getElementById('modal-cost')
 const freeCost = data.data.pricing.map((price) => `
-<div style="height: 100px; width: 120px; background-color: rgb(255, 255, 255);" class="text-center d-flex align-items-center justify-content-center p-2">${price.price}<br>${price.plan}</div>
+<div style="height: 100px;  width: 120px; background-color: rgb(255, 255, 255);" class="text-center d-flex align-items-center justify-content-center p-2">${price.price}<br>${price.plan}</div>
 `)
  modalCost.innerHTML = freeCost.join('')
 
@@ -41,6 +73,11 @@ const firstTitle = document.getElementById('first-title')
 const modalDescription = document.getElementById('second-title')
 firstTitle.innerHTML = data.data.tool_name
 modalDescription.innerHTML = data.data.description
+
+// accuracy
+document.querySelector('.accuracy').innerHTML = Math.parseFloat(data.data.accuracy.score)
+
+
 }
 
 
@@ -52,19 +89,18 @@ const showDetails = id =>{
 }
 
 
-
-
 const displayApi = data => {
   // console.log(data);
+  document.getElementById('spinner').classList.add('d-none');   //spinner-1
   // step 1 : container element
   const toolscontainer = document.getElementById('tools-container');
+  toolscontainer.innerHTML = '';
   data.forEach(tool => {
     // console.log(tool)
     // step 2: create child for each element
     const toolDiv = document.createElement('div');
-    toolDiv.classList.add('col');
+    toolDiv.classList.add('col-12', 'col-md-4');
     // step-3 set content of the child 
-
     toolDiv.innerHTML = `
         <div class="card h-100">
         <img src="${tool.image}" class="card-img-top" alt="...">
@@ -92,7 +128,10 @@ const displayApi = data => {
       //  document.getElementById('showDetail').addEventListener()
 
     toolscontainer.appendChild(toolDiv);
+    
+    
   })
+  
 }
 
 loadApi();
